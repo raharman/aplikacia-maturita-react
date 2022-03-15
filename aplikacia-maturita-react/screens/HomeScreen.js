cbfc3782892ecf8c4f490bef9ea73c54c4393ad7
radoslav.harbulak <r.harbulak3@gmail.com>
2022-03-15 22:20:24 +0100

    vypis dat

--- aplikacia-maturita-react/screens/HomeScreen.js
+++ aplikacia-maturita-react/screens/HomeScreen.js
@@ -1,12 +1,26 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-import { auth } from '../firebase'
-import { useNavigation } from '@react-navigation/native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { auth, db } from '../firebase';
 import { signOut } from "firebase/auth";
+import { collection, getDocs } from 'firebase/firestore';
 
 const HomeScreen = () => {
 
-  const navigation = useNavigation()
+  const navigation = useNavigation();
+
+  const [topics, setTopics] = useState([]);
+  const topicsCollectionRef = collection(db, "topics");
+
+  useEffect(() => {
+    
+    const getTopics = async () => {
+      const data = await getDocs(topicsCollectionRef);
+      setTopics(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+    };
+
+    getTopics()
+  }, [])
 
   const SignOut = () => {
     signOut(auth)
@@ -27,6 +41,17 @@ const HomeScreen = () => {
       >
         <Text style={styles.buttonText}>Sign Out</Text>
       </TouchableOpacity>
+      <View>
+      {topics.map((topic) => { 
+          return(
+            <Text>
+              <h1>Meno: {topic.name}</h1>
+              <p>Text: {topic.text}</p>
+              <p>Typ: {topic.type}</p>
+            </Text>
+          );
+        })}
+      </View>
     </View>
   )
 }
