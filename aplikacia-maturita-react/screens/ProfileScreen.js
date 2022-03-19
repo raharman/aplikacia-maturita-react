1b7e7dc300677853d4e6a3893360d3ecb5eb2450
V.Jančár <jvladko@gmail.com>
2022-03-20 00:57:33 +0100

    profil page provizorne UI
    Signed-off-by: V.Jančár <jvladko@gmail.com>

--- aplikacia-maturita-react/screens/ProfileScreen.js
+++ aplikacia-maturita-react/screens/ProfileScreen.js
@@ -1,14 +1,137 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { auth } from "../firebase";
+import { signOut } from "firebase/auth";
+import { useNavigation } from "@react-navigation/native";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import React from "react";
 
 const ProfileScreen = () => {
+  const navigation = useNavigation();
+
+  const SignOut = () => {
+    signOut(auth)
+      .then(() => {
+        navigation.navigate("Uvod");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View style={styles.container}>
+      <View style={styles.head_rect}>
+        <Text style={styles.head_text}>Profil</Text>
+      </View>
+      <View style={styles.rect}>
+        <Image
+          source={require("../assets/images/profilovka.jpg")}
+          resizeMode="contain"
+          style={styles.image}
+        ></Image>
+        <Text style={styles.text}>Meno Priezvisko</Text>
+      </View>
+      <View>
+        <View style={styles.margin}>
+          <Text style={styles.text2}>Počet bodov:</Text>
+          <View style={styles.rect2}>
+            <Text style={styles.text2}>xxx</Text>
+          </View>
+        </View>
+        <View style={styles.margin}>
+          <Text style={styles.text2}>Škola:</Text>
+          <View style={styles.rect2}>
+            <Text style={styles.text2}>xxx</Text>
+          </View>
+        </View>
+        <View style={styles.margin}>
+          <Text style={styles.text2}>Vek:</Text>
+          <View style={styles.rect2}>
+            <Text style={styles.text2}>xxx</Text>
+          </View>
+        </View>
+      </View>
+      <TouchableOpacity style={styles.rect3} onPress={SignOut}>
+        <Text style={styles.text3}>Odhlásiť sa</Text>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default ProfileScreen
+export default ProfileScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  head_rect: {
+    width: "100%",
+    height: "7%",
+    backgroundColor: "rgba(74,122,150,1)",
+
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    marginTop: "5%",
+
+    marginBottom: "6%",
+  },
+  head_text: {
+    fontFamily: "roboto-700",
+    textAlign: "center",
+    fontSize: "300%",
+    color: "white",
+  },
+  rect: {
+    width: "75%",
+    height: "25%",
+    backgroundColor: "rgba(215,215,215,1)",
+    borderRadius: 19,
+    marginVertical: "6%",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    padding: "5%",
+  },
+  text: {
+    fontFamily: "inter-600",
+    textAlign: "center",
+    fontSize: "150%",
+  },
+  rect2: {
+    width: "auto",
+    height: "auto",
+    backgroundColor: "rgba(74,122,150,10)",
+    padding: "1%",
+    borderRadius: 19,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text2: {
+    fontFamily: "inter-600",
+    fontSize: "120%",
+  },
+  image: {
+    width: "100%",
+    height: "50%",
+  },
+  rect3: {
+    width: "45%",
+    height: "7%",
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+    backgroundColor: "rgba(44,92,120,1)",
+    borderRadius: 19,
+    marginTop: "6%",
+  },
+  text3: {
+    fontFamily: "inter-600",
+    textAlign: "center",
+    fontSize: "150%",
+    color: "white",
+  },
+  margin: {
+    marginVertical: "5%",
+  },
+});
